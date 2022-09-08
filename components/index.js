8a392c8f378d1592d547bc94b0e724f50a837d17
alekskimeu <kim3ual3x@gmail.com>
2022-09-08 16:26:26 +0300

    Created portfolio page

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 export { default as Contact } from "./contact/Contact";
 export { default as Layout } from "./layout/Layout";
 export { default as Hero } from "./hero/Hero";
+export { default as Project } from "./project/Project";
 export { default as Projects } from "./projects/Projects";
 export { default as Skills } from "./skills/Skills";
 export { default as Stats } from "./stats/Stats";
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Layout, Project } from "../components";
+
+import { projects } from "../data/projects";
+
+import styles from "../styles/Projects.module.css";
+
 
 const portfolio = () => {
-	return <div>portfolio</div>;
+	return (
+		<Layout>
+			<section className={styles.projects}>
+				<div className={styles.circle}></div>
+				<div className={styles.donut}></div>
+				<div className={styles.container}>
+					{projects.map((project) => (
+						<Project key={project.index} project={project} />
+					))}
+				</div>
+			</section>
+		</Layout>
+	);
 };
 
 export default portfolio;
